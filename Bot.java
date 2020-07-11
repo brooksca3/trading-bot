45c6a275122e07fbdeb3dfd0faeaffd7d4004647
Henrique <henrique.schechter@gmail.com>
2020-07-11 12:34:21 -0400

    penny

--- Bot.java
+++ Bot.java
@@ -1,51 +1,56 @@
-
 /* HOW TO RUN
    1) Configure things in the Configuration class
    2) Compile: javac Bot.java
    3) Run in loop: while true; do java Bot; sleep 1; done
 */
-import java.lang.*;
-import java.io.PrintWriter;
-import java.io.InputStreamReader;
+
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 class Configuration {
     String exchange_name;
-    int    exchange_port;
+    int exchange_port;
     /* 0 = prod-like
        1 = slow
        2 = empty
     */
-    final Integer test_exchange_kind = 2;
+    final Integer test_exchange_kind = 1;
     /* replace REPLACEME with your team name! */
-    final String  team_name          = "PROSPECTAVENUE";
+    final String team_name = "PROSPECTAVENUE";
 
     Configuration(Boolean test_mode) {
-        if(!test_mode) {
-	    /* If we pass in false, we're putting our bot into PRODUCTION */
+        if (!test_mode) {
+            /* If we pass in false, we're putting our bot into PRODUCTION */
             exchange_port = 20000;
             exchange_name = "production";
         } else {
-	    /* If we pass in true, we're TESTING */
+            /* If we pass in true, we're TESTING */
             exchange_port = 20000 + test_exchange_kind;
             exchange_name = "test-exch-" + this.team_name;
         }
     }
 
-    String  exchange_name() { return exchange_name; }
-    Integer port()          { return exchange_port; }
+    String exchange_name() {
+        return exchange_name;
+    }
+
+    Integer port() {
+        return exchange_port;
+    }
 }
 
-public class Bot
-{
-    public static void main(String[] args)
-    {
+public class Bot {
+
+    public static int orderid = 0;
+    public static int bonds = 0;
+
+    public static void main(String[] args) {
         /* The boolean passed to the Configuration constructor dictates whether or not the
            bot is connecting to the prod or test exchange. Be careful with this switch! */
         Configuration config = new Configuration(true);
-        try
-        {
+        try {
             Socket skt = new Socket(config.exchange_name(), config.port());
             BufferedReader from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
             PrintWriter to_exchange = new PrintWriter(skt.getOutputStream(), true);
@@ -59,11 +64,27 @@ public class Bot
             to_exchange.println(("HELLO " + config.team_name).toUpperCase());
             String reply = from_exchange.readLine().trim();
             System.err.printf("The exchange replied: %s\n", reply);
-        }
-        catch (Exception e)
-        {
+            while (true) {
+                if (bonds < 100) {
+                    to_exchange.println("ADD " + orderid++ + " BOND BUY 999 " + (100 - bonds));
+                    bonds += 100;
+                } else if (bonds > -100) {
+                    to_exchange.println("ADD " + orderid++ + " BOND SELL 1001 " + (bonds));
+                    bonds -= 100;
+                }
+                String ans = from_exchange.readLine().trim();
+                System.err.printf("The exchange replied: %s\n", ans);
+                Thread.sleep(1);
+            }
+        } catch (Exception e) {
             e.printStackTrace(System.out);
         }
     }
+
+    //public static void penny() {
+
+    //}
+
+
 }
 
